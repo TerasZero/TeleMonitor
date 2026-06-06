082b4924a4fdef0bf72b839ff0285c63b3acbb0d
PulsarSight <abebeteraa@gmail.com>
2026-06-06 12:14:30 -0400

    working on collect info, not complete

--- ProcessViewer.cpp
+++ ProcessViewer.cpp
@@ -1,7 +1,27 @@
 #include "ProcessViewer.h"
 
 void procMonitor::collectInfo(){
-	cout<<"test"<<endl;
+	DIR *dirH;
+	struct direct *entries;
+	vector<string> proclist;
+	dirH = opendir("/proc");
+	bool isProc = true;
+	if(dirH == NULL){
+		return;
+	}
+	entries = readdir(dirH);
+	while(entries != NULL){
+		string name = entries-> d_name;
+		for(char c :name){//filters out any dir with letters
+			if(!isdigit(c)){
+				isProc = false;
+			}
+		}
+		if(isProc){//enters in the procs
+			proclist.push_back(name);
+		}
+	}
+	closedir(dirH);
 }
 
 int main(){
